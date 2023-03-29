e6aaaad6935e2b41b3054b2c8a93c6b9ba934def
mataharimau <audrey.tam@gmail.com>
2023-03-29 15:09:56 +1100

    Initial Commit

---
+++ TamTIL/TamTILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamTILApp.swift
+//  TamTIL
+//
+//  Created by Audrey Tam on 29/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamTILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
